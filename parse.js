49504e394a79e74f607b5b11a2eae6560cf24143
Serkan Yersen <serkanyersen@gmail.com>
2017-11-10 01:47:29 -0800

    update code

--- parse.js
+++ parse.js
@@ -1,126 +1,120 @@
 'use strict';
 
-var strip = require('strip-json-comments');
-var tag = /^\@(self|ext)[\.\[]/;
-var selfTag = /^\@self[\.\[]/;
-var fs = require('fs');
-var _ = require('lodash');
+const strip = require('strip-json-comments');
+const tag = /^@(self|ext)[.[]/;
+const selfTag = /^@self[.[]/;
+const fs = require('fs');
+const _ = require('lodash');
 
-var jsonPlus = {
-    /**
-     * Resolves JS array notation in string to actual value
-     * @param  {Object} object    Actual Object
-     * @param  {string} reference path defined in string
-     * @return {any}              matched value or undefined when not found
-     */
-    resolvePath: function(object, reference) {
-        return _.propertyOf(object)(reference);
-    },
+const jsonPlus = {
+  /**
+   * Resolves JS array notation in string to actual value
+   * @param  {Object} object    Actual Object
+   * @param  {string} reference path defined in string
+   * @return {any}              matched value or undefined when not found
+   */
+  resolvePath: function (object, reference) {
+    return _.propertyOf(object)(reference);
+  },
 
-    /**
-     * Simple template tag matcher, finds tags and
-     * evalues values
-     * @param  {object} object   Object itself to resolve tags agains
-     * @param  {string} template String to look for tags
-     * @return {string}          Evaluated string
-     */
-    parseTemplate: function(object, template) {
-        var html = template || '';
-        return html.replace(/\{\{\s*(.*?)\s*\}\}/gim, function(all, match) {
-            return this.resolvePath(object, match.replace(selfTag, '')) || all;
-        }.bind(this));
-    },
+  /**
+   * Simple template tag matcher, finds tags and
+   * evalues values
+   * @param  {object} object   Object itself to resolve tags agains
+   * @param  {string} template String to look for tags
+   * @return {string}          Evaluated string
+   */
+  parseTemplate: function (object, template) {
+    const html = template || '';
+    return html.replace(/\{\{\s*(.*?)\s*\}\}/gim, function (all, match) {
+      return this.resolvePath(object, match.replace(selfTag, '')) || all;
+    }.bind(this));
+  },
 
-    /**
-     * Recursively goes through JSON object and resolves
-     * all self references
-     * @param  {Object} obj  JSON Object part
-     * @param  {Object} self Full object
-     */
-    resolve: function(obj, self, path) {
-        self = self || obj;
-        path = path || [];
+  /**
+   * Recursively goes through JSON object and resolves
+   * all self references
+   * @param  {Object} obj  JSON Object part
+   * @param  {Object} self Full object
+   */
+  resolve: function (obj, self, path) {
+    self = self || obj;
+    path = path || [];
 
-        // If object go through each value and call self again
-        if (_.isPlainObject(obj)) {
-            Object.keys(obj).forEach(function(key) {
-                this.resolve(obj[key], self, path.concat([key]));
-            }.bind(this));
-        }
-
-        // if array go through each item and call self
-        else if (Array.isArray(obj)) {
-            obj.forEach(function(value, index) {
-                this.resolve(obj[index], self, path.concat([index]));
-            }.bind(this));
-        }
-
-        // if string and starts with the reference tag
-        // evaluate and return new value
-        else if (typeof obj === 'string' && tag.test(obj)) {
-            _.set(self, path, this.resolvePath(self, obj.replace(selfTag, '')));
-        }
-
-        // if string check for template tags
-        else if (typeof obj === 'string') {
-            _.set(self, path, this.parseTemplate(self, obj));
-        }
-    },
-
-    /**
-     * Finds all references external JSON objects parses them recursively
-     * and references back to given object
-     * @param  {Object} obj to check against references
-     * @return {Object}     Parsed object
-     */
-    findExternal: function(obj) {
-        if ('@ext' in obj) {
-            Object.keys(obj['@ext']).forEach(function(key) {
-                var file = obj['@ext'][key];
-                var extObj = this.jsonPlusParse(fs.readFileSync(file, 'utf8'));
-                obj['@ext'][key] = extObj;
-            }.bind(this));
-        }
-        return obj;
-    },
-
-    /**
-     * Remove the @external files object
-     * @param  {Object} obj JSON Object
-     * @return {Object}     JSON Object
-     */
-    cleanExternal: function(obj) {
-        if (obj) {
-            delete obj['@ext'];
-        }
-        return obj;
-    },
-
-    /**
-     * Parses json string by removing comments and resolving self references
-     * @param  {string} data JSON string
-     * @return {Object}      parsed JSON Object
-     */
-    jsonPlusParse: function(data) {
-        var obj = JSON.parse(strip(data));
-        obj = this.findExternal(obj);
-        this.resolve(obj);
-        obj = this.cleanExternal(obj);
-        return obj;
-    },
-
-    /**
-     * Resolves already parsed json object
-     * @param  {Object} obj  Already parsed object
-     * @param  {Object} self [Optional] Object to use as reference
-     * @return {Object}      Resolved object
-     */
-    jsonPlusResolve: function(obj, self){
-        obj = this.findExternal(obj);
-        this.resolve(obj, self);
-        obj = this.cleanExternal(obj);
-        return obj;
+    // If object go through each value and call self again
+    if (_.isPlainObject(obj)) {
+      Object.keys(obj).forEach(function (key) {
+        this.resolve(obj[key], self, path.concat([key]));
+      }.bind(this));
+    } else if (Array.isArray(obj)) {
+      // if array go through each item and call self
+      obj.forEach(function (value, index) {
+        this.resolve(obj[index], self, path.concat([index]));
+      }.bind(this));
+    } else if (typeof obj === 'string' && tag.test(obj)) {
+      // if string and starts with the reference tag
+      // evaluate and return new value
+      _.set(self, path, this.resolvePath(self, obj.replace(selfTag, '')));
+    } else if (typeof obj === 'string') {
+      // if string check for template tags
+      _.set(self, path, this.parseTemplate(self, obj));
     }
+  },
+
+  /**
+   * Finds all references external JSON objects parses them recursively
+   * and references back to given object
+   * @param  {Object} obj to check against references
+   * @return {Object}     Parsed object
+   */
+  findExternal: function (obj) {
+    if ('@ext' in obj) {
+      Object.keys(obj['@ext']).forEach(function (key) {
+        const file = obj['@ext'][key];
+        const extObj = this.jsonPlusParse(fs.readFileSync(file, 'utf8'));
+        obj['@ext'][key] = extObj;
+      }.bind(this));
+    }
+    return obj;
+  },
+
+  /**
+   * Remove the @external files object
+   * @param  {Object} obj JSON Object
+   * @return {Object}     JSON Object
+   */
+  cleanExternal: function (obj) {
+    if (obj) {
+      delete obj['@ext'];
+    }
+    return obj;
+  },
+
+  /**
+   * Parses json string by removing comments and resolving self references
+   * @param  {string} data JSON string
+   * @return {Object}      parsed JSON Object
+   */
+  jsonPlusParse: function (data) {
+    let obj = JSON.parse(strip(data));
+    obj = this.findExternal(obj);
+    this.resolve(obj);
+    obj = this.cleanExternal(obj);
+    return obj;
+  },
+
+  /**
+   * Resolves already parsed json object
+   * @param  {Object} obj  Already parsed object
+   * @param  {Object} self [Optional] Object to use as reference
+   * @return {Object}      Resolved object
+   */
+  jsonPlusResolve: function (obj, self) {
+    obj = this.findExternal(obj);
+    this.resolve(obj, self);
+    obj = this.cleanExternal(obj);
+    return obj;
+  },
 };
 
 exports.parse = jsonPlus.jsonPlusParse.bind(jsonPlus);
--- tests/tests.js
+++ tests/tests.js
@@ -1,150 +1,149 @@
 'use strict';
-var parse = require('../parse').parse,
-    fs = require('fs');
+const parse = require('../parse').parse;
+const fs = require('fs');
 
-function fixture(name) {
-    return fs.readFileSync('tests/fixtures/' + name + '.json', 'utf8');
+function fixture (name) {
+  return fs.readFileSync('tests/fixtures/' + name + '.json', 'utf8');
 }
 
 exports.Basic = {
-    testSimpleSelfReference: function(test) {
-        var result = parse(fixture('simple-self-reference'));
+  testSimpleSelfReference: function (test) {
+    const result = parse(fixture('simple-self-reference'));
 
-        test.equal(result.bar, 5, 'bar should be equal to foo');
-        test.done();
-    },
+    test.equal(result.bar, 5, 'bar should be equal to foo');
+    test.done();
+  },
 
-    testComments: function(test) {
-        var result = parse(fixture('comments'));
+  testComments: function (test) {
+    const result = parse(fixture('comments'));
 
-        test.equal(result.bar, 5, 'bar should be equal to foo');
-        test.equal(result.list[0], 5, 'list.0 should be equal to foo');
-        test.done();
-    }
+    test.equal(result.bar, 5, 'bar should be equal to foo');
+    test.equal(result.list[0], 5, 'list.0 should be equal to foo');
+    test.done();
+  },
 };
 
 exports.Templates = {
-    testTemplates: function(test) {
-        var result = parse(fixture('template-tags'));
+  testTemplates: function (test) {
+    const result = parse(fixture('template-tags'));
 
-        test.equal(result.full, 'john doe', 'should create full name');
-        test.done();
-    },
+    test.equal(result.full, 'john doe', 'should create full name');
+    test.done();
+  },
 
-    testTemplatesWithoutPrefix: function(test) {
-        var result = parse(fixture('template-tags'));
+  testTemplatesWithoutPrefix: function (test) {
+    const result = parse(fixture('template-tags'));
 
-        test.equal(result.short, 'john doe', 'should create full name');
-        test.equal(result.detailed, 'hello world', 'should create sentence');
-        test.done();
-    },
+    test.equal(result.short, 'john doe', 'should create full name');
+    test.equal(result.detailed, 'hello world', 'should create sentence');
+    test.done();
+  },
 
-    testTemplatesWithoutPrefixArray: function(test) {
-        var result = parse(fixture('template-tags2'));
+  testTemplatesWithoutPrefixArray: function (test) {
+    const result = parse(fixture('template-tags2'));
 
-        test.equal(result[0].full, 'john doe', 'should create full name');
-        test.done();
-    }
+    test.equal(result[0].full, 'john doe', 'should create full name');
+    test.done();
+  },
 };
 
 exports.ExternalFiles = {
-    testSimpleExternalFile: function(test) {
-        var result = parse(fixture('external-file'));
+  testSimpleExternalFile: function (test) {
+    const result = parse(fixture('external-file'));
 
-        test.equal(result.name, 'john');
-        test.equal(result['@ext'], undefined);
-        test.done();
-    },
+    test.equal(result.name, 'john');
+    test.equal(result['@ext'], undefined);
+    test.done();
+  },
 
-    testDeepExternalFile: function(test) {
-        var result = parse(fixture('external-file2'));
+  testDeepExternalFile: function (test) {
+    const result = parse(fixture('external-file2'));
 
-        test.equal(result.name, 'john');
-        test.equal(result['@ext'], undefined);
-        test.done();
-    },
+    test.equal(result.name, 'john');
+    test.equal(result['@ext'], undefined);
+    test.done();
+  },
 
-    testExtWrongPath: function(test) {
-        test.expect(1);
-        try {
-            parse(fixture('external-file-wrong-path'));
-        } catch(e) {
-            test.ok(true);
-        }
-        test.done();
-    },
-
-    testExternalFileComplex: function(test) {
-        var result = parse(fixture('external-file-complex'));
-        test.equal(result.read1, 'john doe');
-        test.equal(result.read2, 'hello world');
-        test.equal(result.read3, 'yes');
-        test.done();
+  testExtWrongPath: function (test) {
+    test.expect(1);
+    try {
+      parse(fixture('external-file-wrong-path'));
+    } catch (e) {
+      test.ok(true);
     }
+    test.done();
+  },
+
+  testExternalFileComplex: function (test) {
+    const result = parse(fixture('external-file-complex'));
+    test.equal(result.read1, 'john doe');
+    test.equal(result.read2, 'hello world');
+    test.equal(result.read3, 'yes');
+    test.done();
+  },
 };
 
 exports.Complex = {
-    testFirstLevelArray: function(test) {
-        var result = parse(fixture('first-level-array'));
+  testFirstLevelArray: function (test) {
+    const result = parse(fixture('first-level-array'));
 
-        test.equal(result[0].full, 'john doe', 'should create full name');
-        test.equal(result[0].self, 'john', 'should get first name');
-        test.done();
-    },
+    test.equal(result[0].full, 'john doe', 'should create full name');
+    test.equal(result[0].self, 'john', 'should get first name');
+    test.done();
+  },
 
-    testFirstLevelArrayTemplates: function(test) {
-        var result = parse(fixture('first-level-array-templates'));
+  testFirstLevelArrayTemplates: function (test) {
+    const result = parse(fixture('first-level-array-templates'));
 
-        test.equal(result[1].fullName, 'Mr. john doe', 'should get first name');
-        test.done();
-    },
+    test.equal(result[1].fullName, 'Mr. john doe', 'should get first name');
+    test.done();
+  },
 
-    testNotFoundTags: function(test) {
-        var result = parse(fixture('not-found'));
-        test.equal(result.tag, '{{ blah }}');
-        test.done();
-    },
+  testNotFoundTags: function (test) {
+    const result = parse(fixture('not-found'));
+    test.equal(result.tag, '{{ blah }}');
+    test.done();
+  },
 
-    testNotFoundReference: function(test) {
-        var result = parse(fixture('not-found'));
-        test.equal(result.self, undefined);
-        test.done();
-    },
+  testNotFoundReference: function (test) {
+    const result = parse(fixture('not-found'));
+    test.equal(result.self, undefined);
+    test.done();
+  },
 
-    testComplexTags: function(test) {
-        var result = parse(fixture('complex-tags'));
-        test.equal(result.nospaces, 'john');
-        test.equal(result.spaces, 'john');
-        test.equal(result.spacesontheside, 'john');
-        test.equal(result.nonkey, 'api');
-        test.equal(result.hard, 'meh');
-        test.equal(result.fullkeys, 'john');
-        test.done();
-    },
+  testComplexTags: function (test) {
+    const result = parse(fixture('complex-tags'));
+    test.equal(result.nospaces, 'john');
+    test.equal(result.spaces, 'john');
+    test.equal(result.spacesontheside, 'john');
+    test.equal(result.nonkey, 'api');
+    test.equal(result.hard, 'meh');
+    test.equal(result.fullkeys, 'john');
+    test.done();
+  },
 
-    testDeepLink: function(test) {
-        var result = parse(fixture('deep-link'));
-        test.equal(result.andthis.will[0].find.that[0][0][0].link, 'yes');
-        test.done();
-    },
+  testDeepLink: function (test) {
+    const result = parse(fixture('deep-link'));
+    test.equal(result.andthis.will[0].find.that[0][0][0].link, 'yes');
+    test.done();
+  },
 
-    testRefORef: function(test) {
-        var result = parse(fixture('reference-to-reference'));
-        test.equal(result.myUsers, 'jane');
-        test.equal(result.extUser, 'john');
-        test.done();
-    },
+  testRefORef: function (test) {
+    const result = parse(fixture('reference-to-reference'));
+    test.equal(result.myUsers, 'jane');
+    test.equal(result.extUser, 'john');
+    test.done();
+  },
 
-    testFullExample: function(test) {
-        var result = parse(fixture('all-out'));
-        test.equal(result[0].read1, 'simple');
-        test.equal(result[0].read2, 'myapi');
-        test.deepEqual(result[0].read3, { 'first': 'john' });
-        // tags
-        test.equal(result[0].read4, 'simple');
-        test.equal(result[0].read5, 'myapi');
-        test.equal(result[0].read6, '[object Object]');
-        test.done();
-    }
+  testFullExample: function (test) {
+    const result = parse(fixture('all-out'));
+    test.equal(result[0].read1, 'simple');
+    test.equal(result[0].read2, 'myapi');
+    test.deepEqual(result[0].read3, { 'first': 'john' });
+    // tags
+    test.equal(result[0].read4, 'simple');
+    test.equal(result[0].read5, 'myapi');
+    test.equal(result[0].read6, '[object Object]');
+    test.done();
+  },
 };
-
